0916676da09139d3de5d4eadbcd9206338b402c9
Mac <mac@MacBook-Pro.fios-router.home>
2019-01-31 21:31:03 -0500

    chat app

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
     console.log('a user connected')
+ 
     socket.on('disconnect', function(){
         console.log('user disconnected');
+    }).on('chat message', function(msg) {
+        socket.emit('chat message', msg);
+        console.log('message: '+msg);
     })
 });
 
